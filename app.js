6609c3c38166fc7e7047519ce6637f2568db72f8
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-08-02 19:26:14 -0400

    added logic

--- app.js
+++ app.js
@@ -1,57 +1,82 @@
 var energy=80;
 var hunger=50;
 
-var angry_sad = `<img id="tama" src="images/angry-sad.gif" alt="happy tama">`;
+var angry_sad = `<img class="tama" src="images/angry-sad.gif" alt="happy tama">`;
 
-var sleepy = `<img id="tama" src="images/sleepy.gif" alt="happy tama">`;
+var sleepy = `<img class="tama" src="images/sleepy.gif" alt="happy tama">`;
 
-var playing = `<img id="tama" src="images/playing.gif" alt="happy tama">`;
+var playing = `<img class="tama" src="images/playing.gif" alt="happy tama">`;
 
-var satisfied = `<img id="tama" src="images/satisfied.gif" alt="happy tama">`;
+var satisfied = `<img class="tama" src="images/satisfied.gif" alt="happy tama">`;
 
-var happy = `<img id="tama" src="images/happy.gif" alt="happy tama">`;
+var happy = `<img class="tama" src="images/happy.gif" alt="happy tama">`;
+
+var sleeping = `<img class="tama sleep" src="images/sleeping.gif" alt="happy tama">`
 
 function feed(){
-    if(energy < 100 && energy >= 40){
-        energy += 10;
+    if(energy < 100 && energy >= 40 ){
+        energy += 5;
+        hunger += 10;
         document.getElementById("tama").innerHTML=happy;
-        display();
+    
     }
     else if(energy < 40){
-        energy += 10;
+        energy += 5;
+        hunger += 10;
         document.getElementById("tama").innerHTML=sleepy;
     }
-    else if(energy == 100){
+    else if(energy == 100 || hunger == 100){
         document.getElementById("tama").innerHTML=satisfied;
-        display();
+    
     }
     // console.log(energy);
-    display();
+
 };
 
 function play(){
-    if(energy <= 100 && energy > 40){
+    if(energy <= 100 && energy > 40 && hunger > 20){
         energy -= 20;
+        hunger -= 10;
         document.getElementById("tama").innerHTML=playing;
-        display();
+    
     }
-    else if(energy < 50 && energy > 20){
+    else if(energy < 50 && energy > 20 && hunger > 20){
         energy -= 20;
+        hunger -= 10;
         document.getElementById("tama").innerHTML=sleepy;
-        display()
+    }
+    else if(energy <= 20 || hunger <= 20){
+        document.getElementById("tama").innerHTML=angry_sad;
+    }
+};
+
+function sleep(){
+    if(energy != 100){
+        energy = 100;
+        document.getElementById("tama").innerHTML=sleeping;
+    }
+}
+
+function backToTama(){
+    if(energy > 40){
+        document.getElementById("tama").innerHTML=happy;
+    }
+    else if(energy <= 40 && energy > 20){
+        document.getElementById("tama").innerHTML=sleepy;
     }
     else if(energy <= 20){
         document.getElementById("tama").innerHTML=angry_sad;
-        display()
     }
-    display();
-};
+}
 
 function display(){
-    document.getElementById("info").innerHTML=`<p>Energy: ${energy}</p>`;
+    // document.getElementById("info").innerHTML=`<p>Energy: ${energy}</p>`;
+    document.getElementById("tama").innerHTML=`
+    <p>Energy: ${energy}</p>
+    <p>Hunger: ${hunger}</p>
+    `;
 };
 
-display();
 
 // add level of hunger
 // add sleep button.
